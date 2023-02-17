b2169f37d9590e2a9f278e9dfd5b229e5a45ae04
Lucas Reis <lucasreis_9@hotmail.com>
2023-02-17 18:57:38 -0300

    TextField e Decoration

--- lib/main.dart
+++ lib/main.dart
@@ -18,14 +18,50 @@ class _HomeState extends State<Home> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text("Calculadora de IMC"),
-        centerTitle: true,
-        backgroundColor: Colors.green,
-        actions: [
-          IconButton(
-            onPressed: () {},
-            icon: Icon(Icons.refresh),
+          title: Text("Calculadora de IMC"),
+          centerTitle: true,
+          backgroundColor: Colors.green,
+          actions: [
+            IconButton(
+              onPressed: () {},
+              icon: Icon(Icons.refresh),
+            )
+          ]),
+      backgroundColor: Colors.white,
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: [
+          Icon(
+            Icons.person_outlined,
+            size: 120,
+            color: Colors.green,
           ),
+          TextField(
+            decoration: InputDecoration(
+              label: Text("Peso (kg)"),
+              labelStyle: TextStyle(
+                color: Colors.green,
+              ),
+            ),
+            textAlign: TextAlign.center,
+            style: TextStyle(
+              color: Colors.green,
+              fontSize: 25,
+            ),
+          ),
+          TextField(
+            decoration: InputDecoration(
+              label: Text("Altura (cm)"),
+              labelStyle: TextStyle(
+                color: Colors.green,
+              ),
+            ),
+            textAlign: TextAlign.center,
+            style: TextStyle(
+              color: Colors.green,
+              fontSize: 25,
+            ),
+          )
         ],
       ),
     );
